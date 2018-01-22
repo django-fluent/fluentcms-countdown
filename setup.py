aac533c5360aa07a76f76ac37b3c958e24f5dea0
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-22 22:36:43 +0100

    update trove classifiers

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
     license='Apache 2.0',
 
     install_requires=[
-        'django-fluent-contents>=1.0',   # Need frontend_media support.
+        'django-fluent-contents>=2.0',
     ],
     requires=[
-        'Django (>=1.4)',
+        'Django (>=1.10)',
     ],
 
     description='A countdown timer for django-fluent-contents',
@@ -59,17 +59,22 @@ setup(
 
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
